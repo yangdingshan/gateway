a91f10b97abb6881d9a3b19d82e4e81ef86c192c
yangdingshan <yangdsh1@sugon.com>
2022-05-23 17:00:16 +0800

    init

---
+++ src/main/java/com/example/gatewaytest/GatewayTestApplication.java
@@ -0,0 +1,15 @@
+package com.example.gatewaytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class GatewayTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gatewaytest/GatewayTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gatewaytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
